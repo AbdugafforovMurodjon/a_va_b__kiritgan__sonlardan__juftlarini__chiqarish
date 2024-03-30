60044e8f7e8ba31046da7b883cd5a8ffc033394e
AbdugafforovMurodjon <ume8156@gamil.com>
2024-03-30 14:59:54 +0500

    juftlarini topish a va b sonlar orasidagi

---
+++ index.js
@@ -0,0 +1,10 @@
+function juftSoniChiqarish(a, b) {
+    for (let i = a; i <= b; i++) {
+        if (i % 2 === 0) {
+            console.log(i);
+        }
+    }
+}
+
+
+juftSoniChiqarish(1, 17); // 4, 6, 8, 10
